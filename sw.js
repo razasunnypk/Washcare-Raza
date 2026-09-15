9ddbdb9018dd3e124861d88cab858fc52563c450
razasunnypk <razasunnypk@gmail.com>
2026-09-15 19:05:37 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Network-first: always fresh when online, still opens when offline. */
 var CACHE = "washcare-v1";
-var ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./icon.png", "./config.js", "./icon-192.png"];
+var ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./icon.png", "./config.js", "./icon-192.png", "./shot-mobile.png", "./shot-wide.png"];
 
 self.addEventListener("install", function(e){
   self.skipWaiting();
